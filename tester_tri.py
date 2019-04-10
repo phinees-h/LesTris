2ad4d951f7f2ea53b6ccd876882f0c305b7815ff
Essi Linda <alindaessi@gmail.com>
2019-04-10 16:54:22 +0100

    ajout du fichier tester_tri.py

---
+++ tester_tri.py
@@ -0,0 +1,6 @@
+from tri import tri_insertion_iteratif, tri_ins_recursif
+table_a_trier1 = [1,0,-3,5]
+table_a_trier2 = [-2,-3,9,-1]
+print(tri_insertion_iteratif(table_a_trier1))
+tri_ins_recursif(table_a_trier2)
+print(table_a_trier2)
